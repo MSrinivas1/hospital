03b384a843bd6eabfbf920278143fee51c8ed2be
MSrinivas1 <mangalipelli.srinivas@gmail.com>
2016-04-02 07:48:02 +0530

    intitial commites

---
+++ hospital/src/main/java/com/chinna/hospital/Test.java
@@ -0,0 +1,9 @@
+package com.chinna.hospital;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ hospital/src/main/java/com/chinna/hospital/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.chinna.hospital.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.chinna.hospital")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setOrder(1);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setRedirectHttp10Compatible(false);
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }  
+}
